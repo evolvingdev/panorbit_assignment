96e7fb95cb3c90e15b4ca6caa4b6be466527fd14
Harsh sharma <evolvingdev10@gmail.com>
2023-08-26 17:23:11 +0530

    warnings fixed

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Users.css";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Users(props) {
   const navigate = useNavigate();
